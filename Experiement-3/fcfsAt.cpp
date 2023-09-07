a8b877e027a721e0e887956a56dbae2204246d4f
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-09-07 18:45:42 +0530

    Made a few changes to FCFS prints the proper decimmal output values

--- Experiement-3/fcfsAt.cpp
+++ Experiement-3/fcfsAt.cpp
@@ -1,15 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define N 30
 
 using namespace std;
 
-struct p{
+struct p
+{
     int at, bt, wt, tat, p_no;
 };
 
 bool cmp(p a, p b)
 {
-    return (a.at <  b.at);
+    return (a.at < b.at);
 }
 
 bool cmp_id(p a, p b)
@@ -19,81 +20,83 @@ bool cmp_id(p a, p b)
 
 int main(int argc, char const *argv[])
 {
-    int n, atat=0, awt=0;
+    int n;
+    float atat = 0, awt = 0;
     struct p pro[N];
-    cout<<"Enter the number of processes: ", cin>>n;    
+    cout << "Enter the number of processes: ", cin >> n;
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        pro[i].p_no = i+1;
-        cout<<"enter the burst time: ", cin>>pro[i].bt;
-        cout<<"enter the arrival time: ", cin>>pro[i].at;
-        pro[i].wt = pro[i].tat=0;
+        pro[i].p_no = i + 1;
+        cout << "enter the burst time: ", cin >> pro[i].bt;
+        cout << "enter the arrival time: ", cin >> pro[i].at;
+        pro[i].wt = pro[i].tat = 0;
     }
 
     sort(pro, pro + n, cmp);
 
     // calc tats
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        int ct=0;
-        for(int j=0;j<i;j++)
-            ct+=pro[j].bt;
-        ct+=pro[i].bt;
-        pro[i].tat = ct-pro[i].at;
+        int ct = 0;
+        for (int j = 0; j < i; j++)
+            ct += pro[j].bt;
+        ct += pro[i].bt;
+        pro[i].tat = ct - pro[i].at;
     }
 
     // calc wt
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
         pro[i].wt = pro[i].tat - pro[i].bt;
-    
-    // calc atat and awt
-    
-    for(int i=0;i<n;i++)
-        atat+=pro[i].tat, awt+=pro[i].wt;
 
-    sort(pro, pro+n, cmp_id);
+    // calc atat and awt
+
+    for (int i = 0; i < n; i++)
+        atat += pro[i].tat, awt += pro[i].wt;
+
+    sort(pro, pro + n, cmp_id);
 
     // table
-    cout<<"Pro no\tArrival time\t\twaiting time\t\tTAT"<<endl;
-    for(int i=0;i<n;i++)
-        cout<<pro[i].p_no<<"\t"<<pro[i].at<<"\t"<<pro[i].wt<<"\t"<<pro[i].tat<<endl;
+    cout << "Pro no\tAT\tWT\tTAT" << endl;
+    for (int i = 0; i < n; i++)
+        cout << pro[i].p_no << "\t" << pro[i].at << "\t" << pro[i].wt << "\t" << pro[i].tat << endl;
 
-    cout<<"Avg waiting time: "<<awt/n<<endl<<"Avg tat: "<<atat/n<<endl;;
+    cout << "Avg waiting time: " << std::setprecision(4) << awt / n << endl
+         << "Avg tat: " << std::setprecision(4) << atat / n << endl;
+    ;
 
-    sort(pro, pro+n, cmp);
+    sort(pro, pro + n, cmp);
 
     // gantt
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<"|";
-        for(int j=0;j<pro[i].bt;j++)
+        cout << "|";
+        for (int j = 0; j < pro[i].bt; j++)
         {
-            if(j==pro[i].bt/2)
-                cout<<"P"<<pro[i].p_no;
+            if (j == pro[i].bt / 2)
+                cout << "P" << pro[i].p_no;
             else
-                cout<<" ";
+                cout << " ";
         }
     }
-    cout<<"|";
-    cout<<endl;
-    int global_ct=0;
-    cout<<global_ct;
-    for(int i=0;i<n;i++)
+    cout << "|";
+    cout << endl;
+    int global_ct = 0;
+    cout << global_ct;
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<pro[i].bt-1;j++)
-            cout<<" ";
-        global_ct+=pro[i].bt;
-        if(global_ct<10)
-            cout<<"  "<<global_ct;
+        for (int j = 0; j < pro[i].bt - 1; j++)
+            cout << " ";
+        global_ct += pro[i].bt;
+        if (global_ct < 10)
+            cout << "  " << global_ct;
         else
-            cout<<" "<<global_ct;
+            cout << " " << global_ct;
     }
-    cout<<endl;
+    cout << endl;
 
     return 0;
 }
-
 
 /*
 5
