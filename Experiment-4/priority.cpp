82341602cb7d334ee8bf608bf4e49ef25079eee5
DeeptejD <deeptejdhauskar2003@gmail.com>
2024-01-17 19:48:36 +0530

    changes

---
+++ Experiment-4/priority.cpp
@@ -0,0 +1,1 @@
+// pre-emptive priority cpu scheduling
--- Experiment-7/clook.cpp
+++ Experiment-7/clook.cpp
@@ -26,18 +26,26 @@ int CLOOK(int headPosition, int totalRequests, int requests[], bool direction)
     while (direction ? left >= 0 : right < totalRequests)
     {
         if (direction)
-            seekTime += abs(currentPosition - requests[left]), cout << "(Distance: " << abs(currentPosition - requests[left]), currentPosition = requests[left], cout <<", " << currentPosition << ")" << " -> ", left--;
+            seekTime += abs(currentPosition - requests[left]), cout << "(Distance: " << abs(currentPosition - requests[left]), currentPosition = requests[left], cout << ", " << currentPosition << ")"
+                                                                                                                                                                      << " -> ",
+                                                                                                                               left--;
         else
-            seekTime += abs(currentPosition - requests[right]), cout << "(Distance: " << abs(currentPosition - requests[right]), currentPosition = requests[right], cout <<", " << currentPosition << ")" << " -> ", right++;
+            seekTime += abs(currentPosition - requests[right]), cout << "(Distance: " << abs(currentPosition - requests[right]), currentPosition = requests[right], cout << ", " << currentPosition << ")"
+                                                                                                                                                                         << " -> ",
+                                                                                                                                 right++;
     }
 
     // if there are no more requests on the left side, move to the right side
     while (direction ? requests[right] > headPosition : requests[left] < headPosition)
     {
         if (direction)
-            seekTime += abs(currentPosition - requests[right]), cout << "(Distance: " << abs(currentPosition - requests[right]), currentPosition = requests[right], cout <<", " << currentPosition << ")" << " -> ", right--;
+            seekTime += abs(currentPosition - requests[right]), cout << "(Distance: " << abs(currentPosition - requests[right]), currentPosition = requests[right], cout << ", " << currentPosition << ")"
+                                                                                                                                                                         << " -> ",
+                                                                                                                                 right--;
         else
-            seekTime += abs(currentPosition - requests[left]), cout << "(Distance: " << abs(currentPosition - requests[left]), currentPosition = requests[left], cout <<", " << currentPosition << ")" << " -> ", left++;
+            seekTime += abs(currentPosition - requests[left]), cout << "(Distance: " << abs(currentPosition - requests[left]), currentPosition = requests[left], cout << ", " << currentPosition << ")"
+                                                                                                                                                                      << " -> ",
+                                                                                                                               left++;
     }
 
     return seekTime;
@@ -74,4 +82,11 @@ int main(int argc, char const *argv[])
 64
 120
 
+*/
+
+/*
+KEY POINTS THAT MAKE THIS DIFF FROM CSCAN
+1) we dont add input in the first and last
+2) no need to take number of cylinders as the input
+3) when setting right according to direction, set it to n-1 and not n since we aremt starting from the extreme right
 */
